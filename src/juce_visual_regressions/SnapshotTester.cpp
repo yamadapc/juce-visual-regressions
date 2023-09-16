0547c7d5ba6a20e13ceff1f52d8110c8641ba71f
yamadapc <tacla.yamada@gmail.com>
2023-09-17 08:09:40 +1000

    Try to actually run a comparison

--- src/juce_visual_regressions/SnapshotTester.cpp
+++ src/juce_visual_regressions/SnapshotTester.cpp
@@ -39,6 +39,27 @@ void matchesSnapshot(Component& component, std::string_view name) {
     throw std::runtime_error("Could not create file output stream");
   PNGImageFormat png;
   png.writeImageToStream(image, *fileOutputStream);
+
+  const File& previousFilePath = rootDirectory.getChildFile(
+    juce::String::formatted("./test/visual-regression/stable/%s.png", name));
+  previousFilePath.getParentDirectory().createDirectory();
+  if(!previousFilePath.exists()) {
+    filePath.moveFileTo(previousFilePath);
+    return;
+  }
+
+  const File& diffFilePath = rootDirectory.getChildFile(
+    juce::String::formatted("./test/visual-regression/diff/%s.png", name));
+  diffFilePath.getParentDirectory().createDirectory();
+  juce::ChildProcess diffProcess;
+  diffProcess.start("magick compare -metric AE -fuzz 10% \"" +
+                    previousFilePath.getFullPathName() + "\" \"" +
+                    filePath.getFullPathName() + "\" \"" +
+                    diffFilePath.getFullPathName() + "\"");
+
+  assert(diffProcess.waitForProcessToFinish(5000));
+
+  assert(diffProcess.getExitCode() == 0);
 }
 
 void testComponent(const std::function<void()>& test) {
@@ -52,12 +73,13 @@ void testComponent(const std::function<void()>& test) {
   juce::MessageManager::deleteInstance();
 }
 
-void runComponentSnapshotTest(std::string_view name, std::function<Component* ()> test) {
-    testComponent([&]() {
-        auto component = test();
-        matchesSnapshot(*component, name);
-        delete component;
-    });
+void runComponentSnapshotTest(std::string_view name,
+                              std::function<Component*()> test) {
+  testComponent([&]() {
+    auto component = test();
+    matchesSnapshot(*component, name);
+    delete component;
+  });
 }
 
 Snapshot SnapshotTester::takeSnapshot(Component& component) {
